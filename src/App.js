cd507915f1f3c61fa94e5644c011a51ca5f8a28d
Mujtaba <mujtabazaidi0512@gmail.com>
2025-03-24 13:27:22 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { createContext, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -34,3 +35,30 @@ function App() {
 
 
 export default App;
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> 0af4eb9 (Initial commit)
